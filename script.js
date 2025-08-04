eda5c4326584c52bcba080af946a8f5ea475f88c
Zeinab Hosseini <zhzeinabzh@gmail.com>
2025-08-04 09:05:38 -0700

    ini
    ini

---
+++ script.js
@@ -0,0 +1,25 @@
+alert("Hello World!")
+
+let js = 'Amazing'
+console.log (js);
+
+if (js === 'Amazing')
+alert("Javascript is Fun!")
+
+js = 'Boring'
+if (js === 'Amazing')
+alert("Javascript is Fun!")
+
+let sum = 40 + 5 + 9;
+console.log (sum);
+
+
+console.log ("Zeinab");
+console.log(24);
+
+let myName = 'Zeinab';
+console.log (myName);
+
+
+// We can't use numbers at the first of variable names and & sign
+
