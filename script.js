fe93487b741972fa428862d47f571c1c9dab62a0
Zeinab Hosseini <zhzeinabzh@gmail.com>
2025-08-06 09:21:15 -0700

    objects
    objects literal syntax

--- script.js
+++ script.js
@@ -332,7 +332,6 @@ console.log(age1, age2, age3);
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
 
-*/
 
 ///////////////////////////////////////
 // Basic Array Operations (Methods)
@@ -366,4 +365,95 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
   console.log('You have a friend called Steven');
 }
+
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+
+
+
+
+///////
+// Introduction to Objects
+Object literal syntax ................. properity : value
+
+const zeinabArray = [
+  'Zeinab',
+  'Hosseini',
+  2025 - 2000,
+  'mentor',
+  ['Zuhal', 'Setara', 'Parwana']
+];
+
+const zeinab = {
+  firstName: 'Zeinab',
+  lastName: 'Hosseini',
+  age: 2025 - 2000,
+  job: 'mentor',
+  friends: ['Zuhal', 'Setara', 'Parwana']
+};
+
+
+///////////////////////////////////////
+// Dot vs. Bracket Notation for accessing properities (returning)
+
+const zeinab = {
+  firstName: 'Zeinab',
+  lastName: 'Hosseini',
+  age: 2025 - 2000,
+  job: 'mentor',
+  friends: ['Zuhal', 'Setara', 'Parwana']
+};
+
+console.log(zeinab);
+
+console.log(zeinab.lastName);  //Dot
+console.log(zeinab['lastName']); /// Bracket
+
+const nameKey = 'Name';
+console.log(zeinab['first' + nameKey]);
+console.log(zeinab['last' + nameKey]);
+
+// console.log(zeinab.'last' + nameKey)
+
+const interestedIn = prompt('What do you want to know about zeinab? Choose between firstName, lastName, age, job, and friends');
+
+if (zeinab[interestedIn]) {
+  console.log(zeinab[interestedIn]);
+} else {
+  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+zeinab.location = 'Afghanistan';
+zeinab['insta'] = '@zeinab2art';
+console.log(zeinab);
+
+// Challenge
+// "Zeinab has 3 friends, and her best friend is called Zuhal"
+console.log(`${zeinab.firstName} has ${zeinab.friends.length} friends, and her best friend is called ${zeinab.friends[0]}`);
+
+*/
 
